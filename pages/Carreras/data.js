057cb7c92229bdee92144f878e9b979bf91894df
Lummy <67608060+Lummy-rs@users.noreply.github.com>
2022-07-28 18:05:56 +0000

    placeholder data itec

---
+++ pages/Carreras/data.js
@@ -0,0 +1,67 @@
+
+export const data = {
+  
+    "comercio_exterior":{
+        "Tipo":"tecnologico",
+        "Malla": "https://itec.edu.ec/wp-content/uploads/2020/04/Malla-Comercio-Exterior.png",
+        "Descripcion": "La carrera de Comercio de Exterior forma tecnólogos profesionales idóneos que aporten al campo de los negocios con eficacia y calidad, en base a los conocimientos teóricos, prácticos y metodológicos impartidos con una visión lógica y ordenada del proceso de importación y exportación, dominando herramientas técnicas, tecnológicas y de negocios que les permita contribuir con la trasformación económica y productiva del país incentivando el emprendimiento, el análisis y la investigación para mejorar los procesos en base a  estrategias y planes de negocios.",    
+        "Nombre":  "Comercio Exterior",
+    },
+    "Desarrollo_de_Software":{
+        "Nombre": "Desarrollo de Software",
+        "Tipo": "tecnologico" ,
+        "Malla":  "https://itec.edu.ec/wp-content/uploads/2020/10/MALLA-DESARROLLO-DE-SOFTWARE.jpg" ,
+        "Descripcion": "La carrera de desarrollo de software forma tecnólogos profesionales con capacidades críticas, reflexivas y creativas para el desempeño de actividades en relación directa con la tecnología informática de programación bajo parámetros de responsabilidad social, principios éticos y plenamente competitivos con los avances de la ciencia tecnológica, con plena conciencia de integridad humana que aporte a su desarrollo personal y por ende crecimiento social y económico. "
+    },
+    "Estetica_Integral":{
+        "Descripcion":"La Carrera de Estética Integral forma tecnólogos con capacidad para analizar conocimientos científico técnicos que permitan describir el contexto en el que un ser humano se desarrolla, analizándolo desde el punto de vista social, cultural, anatómico, fisiológico e intercultural para poder aplicar procedimientos de belleza, recuperación de tejidos y masofilaxia que promuevan el bienestar físico, mental y social del ser humano íntegro; ejecutando acciones que permitan a los usuarios de los centros estéticos mantener el equilibrio entre la imagen personal, su salud   y las relaciones con su entorno."
+        ,
+        "Malla": "https://itec.edu.ec/wp-content/uploads/2020/01/Malla-Estetica-integral_001.png"
+        ,
+        "Tipo": "tecnologico"
+        ,
+        "Nombre": "Estética Integral" 
+    },
+    "Gestion_de_Riesgos":{
+    
+        "Descripcion": "La carrera de Gestión de Riesgos forma tecnólogos profesionales con la capacidad de realizar y ejecutar procedimientos de prevención, mitigación y recuperación de desastres, en base al análisis de casos de riesgos identificando las amenazas, vulnerabilidades y fortalezas bajo contextos locales y nacionales, de índole natural o no, enfrentando dichos eventos con capacidad de reacción, bajo competencias de organización y liderazgo en el campo de la prevención de la Gestión de Riesgos, a través de su formación teórica, práctica y metodológica."
+        ,
+        "Malla":  "https://itec.edu.ec/wp-content/uploads/2020/03/Malla-Gestion-de-Riesgos.png"
+        ,
+        "Nombre": "Gestión de Riesgos"
+        ,
+        "Tipo":  "tecnologico"
+      
+    },
+    "Guía_Nacional_de_Turismo":{
+      
+        "Descripcion": "La carrera de Guía Nacional de Turismo con nivel equivalente a Técnico Superior forma profesionales con sólidos conocimientos teórico-práctico, con capacidad para administrar y emprender en el sector turístico, en base a procesos que permitan la articulación de las funciones de formación, investigación y vinculación, mejorando la calidad de vida  y contribuyendo al desarrollo del de la Región Sur del Ecuador.",
+        "Malla": "https://itec.edu.ec/wp-content/uploads/2020/12/MALLA-TECNOLOGIA-SUPERIOR-EN-GUIA-NACIONAL-DE-TURISMO-.jpg",
+        "Nombre":  "Guía Nacional de Turismo",
+        "Tipo":  "tecnico"
+    },
+    "Mecanica_Automotriz":{
+      
+        "Malla": "https://itec.edu.ec/wp-content/uploads/2020/12/MALLA-TECNICO-SUPERIOR-EN-MECANICA-AUTOMOTRIZ-.jpg",
+        "Nombre": "Mecánica Automotriz",
+        "Descripcion": "La carrera de Mecánica Automotriz   forma técnicos profesionales con solidos conocimientos teóricos, prácticos y metodológicos en el campo de la mecánica automotriz en relación con el proceso, arreglo, instalación, manipulación, control y mantenimiento preventivo y correctivo de distintos sistemas del automotor, para brindar seguridad al conductor.",
+        "Tipo":  "tecnico"
+     
+    },
+    "Negocios_Agropecuarios":{
+      
+        "Descripcion":  "Formar profesionales en el área de los Negocios Agropecuarios con sólidos conocimientos, teóricos, prácticos y metodológicos en el comercio y expendio de la producción agropecuaria, impartiendo protocolos de compra y venta, y optimizando el proceso de distribución de productos agropecuarios de esta manera se brindara una mejor rentabilidad a los productores, los mismo que generaran un mayor impacto en la sociedad en general.",
+        "Tipo": "tecnologico",
+        "Malla": "https://itec.edu.ec/wp-content/uploads/2020/04/Malla-Negocios-Agropecuarios.png" ,
+        "Nombre":  "Negocios Agropecuarios"
+      
+    },
+    "Talento_Humano":{
+        "Nombre":  "Talento Humano",
+        "Tipo":  "tecnologico",
+        "Malla":  "https://itec.edu.ec/wp-content/uploads/2020/10/Malla-Gestion-del-Talento-Humano.jpeg",
+        "Descripcion": "La carrera de Gestión del Talento Humano forma tecnólogos, con vastos conocimientos en todos los procesos relacionados con la Gestión del Talento Humano, capaces de determinar los diferentes ámbitos de competencias lo que permitirá transformar los procesos en estricto cumplimiento de las disposiciones legales, optimizando los procedimientos, lo que permitirá que el personal de las diferentes instituciones conozca los diferentes aspectos relacionados a la Gestion del Talento Humano y los beneficios que correcta gestión producen las instituciones."
+      
+    }
+  }
+  
